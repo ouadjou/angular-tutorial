2faebde35bddf6a5c5b7b01eeb449e6b8bc29c04
ouadjou <yusf.ouadjou@gmail.com>
2022-08-27 11:45:05 +0100

    T2 desplay a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../moch-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: "windstorm"
-  };
-
+  heroes = HEROES;
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
 }
