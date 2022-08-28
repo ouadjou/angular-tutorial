f032c8b66187094854856e9777353499b0bd325f
ouadjou <yusf.ouadjou@gmail.com>
2022-08-28 12:22:02 +0100

    T4 adding services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../moch-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+
+
+  selectedHero?: Hero;
+  heroes:Hero[]=[];
  
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService:MessageService) { }
 
   ngOnInit(): void {
+  this.getHeroes();
   }
-  selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+}
+getHeroes(): void{
+  this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
 }
 
 }
